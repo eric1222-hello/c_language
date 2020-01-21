cd6e6c6aae930f2b2512e4134c9a3e207bdbb04b
eric1222-hello <58102575+eric1222-hello@users.noreply.github.com>
2020-01-21 12:44:22 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,t;
+    while(cin>>n>>t)
+    {
+        int i,j,tmp;
+        int num[t]={};
+
+        for(i=0;i<t;i++)
+        {
+            cin>>tmp;
+            num[i] += tmp;
+        }
+    }
+}
