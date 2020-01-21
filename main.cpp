aabb9ba8570874ecc8c8964802af605c9729ecd1
eric1222-hello <58102575+eric1222-hello@users.noreply.github.com>
2020-01-21 12:41:04 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,19 +1,50 @@
 #include <iostream>
 
+int judge()
+{
+
+}
+
 using namespace std;
 
 int main()
 {
-    int n,t;
-    while(cin>>n>>t)
+    int n;
+    while(cin>>n)
     {
-        int i,j,tmp;
-        int num[t]={};
-
-        for(i=0;i<t;i++)
+        int i,j;
+        int num[n][n]={};
+        int ans[n][n]={};
+        for(i=0;i<n;i++)
         {
-            cin>>tmp;
-            num[i] += tmp;
+            for(j=0;j<n;j++)
+                cin>>num[i][j];
         }
-    }
+        for(i=0;i<n;i+=2)
+        {
+            for(j=0;j<n;j+=2)
+            {
+                int maxi = num[i][j];
+
+                if(maxi < num[i+1][j])
+                    maxi = num[i+1][j];
+                if(maxi < num[i][j+1])
+                    maxi = num[i][j+1];
+                if(maxi < num[i+1][j+1])
+                    maxi = num[i+1][j+1];
+                ans[i][j] = maxi;
+            }
+        }
+
+        for(i=0;i<n;i+=2)
+        {
+            for(j=0;j<n;j+=2)
+            {
+                cout<<ans[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+
+
+}
 }
