e12cd31e896d145ecf6451f409d8fc24e6001632
eric1222-hello <58102575+eric1222-hello@users.noreply.github.com>
2020-01-21 12:37:16 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,42 +4,16 @@ using namespace std;
 
 int main()
 {
-    int b,p; //b為最少人流之公車站牌，p為示共有多少人要搭乘此公車
-    while(cin>>b>>p)
+    int n,t;
+    while(cin>>n>>t)
     {
-        int i,j,n,m,tmp;
-        int num[1001] = {0};
-        for(i=1;i<=p;i++)
+        int i,j,tmp;
+        int num[t]={};
+
+        for(i=0;i<t;i++)
         {
-            cin>>n>>m;
-            if(n>m)
-            {
-                tmp = n;
-                n = m;
-                m = tmp;
-            }
-            for(j=n;j<=m;j++)
-                num[j]+=1;
+            cin>>tmp;
+            num[i] += tmp;
         }
-        int max_index,min_index;
-        int maxi = 1;
-        int mini = 1000;
-        for(i=1;i<=b;i++)
-        {
-            if(mini > num[i])
-            {
-                mini = num[i];
-                min_index = i;
-            }
-            if(maxi <= num[i])
-            {
-                maxi = num[i];
-                max_index = i;
-            }
-        }
-
-        cout<<min_index<<" "<<max_index<<endl;
-
-
     }
 }
